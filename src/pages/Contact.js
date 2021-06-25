d7484ef7da142db2b93c0c831c231f10c84e24b5
Jared Sutch <sutch002@gmail.com>
2021-06-24 22:48:32 -0500

    saved my files

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function Contact() {
+  handleInputChange();
+  handleFormSubmit();
   return (
     <div>
       <h1>Contact Page</h1>
 
-      {/*<p>Hello {userName}</p>
+      <p>Hello {userName}</p>
       <form className="form">
         <input
           value={email}
@@ -36,7 +38,7 @@ export default function Contact() {
         <div>
           <p className="error-text">{errorMessage}</p>
         </div>
-      )}*/}
+      )}
     </div>
   );
 }
